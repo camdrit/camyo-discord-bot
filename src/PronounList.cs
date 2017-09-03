7a33e52d5526593257c8e2cb64b80da5bf7dbc29
Cam Richardson <cameronrichardsonit@gmail.com>
2017-09-03 15:39:25 -0500

    rename src folder

---
+++ src/PronounList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Camyo
+{
+    public class PronounList
+    {
+        public Dictionary<String, String> pronounsList { get; set; }
+        public Dictionary<String, String> pronounsByName { get; set; }
+        public List<List<object>> pronounTypes { get; set; }
+    }
+}
